787b903dfebcd0a5dc306f864abb7394441ef4e4
shutils <komorebi2614@gmail.com>
2024-03-01 13:46:56 +0900

    refactor: Remove unnecessary files

--- denops/dps-obsidian/main.ts
+++ denops/dps-obsidian/main.ts
@@ -2,6 +2,14 @@ import { Denops, fn, unknownutil as u, vars } from "./deps.ts";
 import { isInVault, openObsidian, syncObsidian } from "./common.ts";
 
 export function main(denops: Denops) {
+  const commands: string[] = [
+    `command! -nargs=0 OpenObsidian call denops#notify('${denops.name}', 'open:note:obsidian', [{'notePath': expand('%:p')}])`,
+    `command! -nargs=0 SyncObsidian call denops#notify('${denops.name}', 'sync:note:obsidian', [{'notePath': expand('%:p')}])`,
+  ];
+
+  commands.map((cmd) => {
+    denops.cmd(cmd);
+  });
   denops.dispatcher = {
     ...denops.dispatcher,
     "open:note:obsidian": async (args: unknown) => {
@@ -9,10 +17,9 @@ export function main(denops: Denops) {
         args,
         u.isObjectOf({ notePath: u.isString }),
       );
-      const cmd = u.ensure(
-        await vars.g.get(denops, "denops_obsidian_cmd"),
-        u.isOptionalOf(u.isUnionOf([u.isString, u.isNull])),
-      );
+      const cmd = await vars.g.get(denops, "denops_obsidian_cmd") as
+        | string
+        | null;
       if (!await isInVault(ensuredArgs.notePath)) {
         return;
       }
@@ -23,10 +30,9 @@ export function main(denops: Denops) {
         args,
         u.isObjectOf({ notePath: u.isString }),
       );
-      const cmd = u.ensure(
-        await vars.g.get(denops, "denops_obsidian_cmd"),
-        u.isOptionalOf(u.isUnionOf([u.isString, u.isNull])),
-      );
+      const cmd = await vars.g.get(denops, "denops_obsidian_cmd") as
+        | string
+        | null;
       if (!await isInVault(ensuredArgs.notePath)) {
         return;
       }
